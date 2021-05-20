f9d584b415f5e1aa029d75fe9510e723d48adc3c
arcbjorn <arky.happ@gmail.com>
2021-05-20 21:13:50 +0300

    perf (cluster): add fault tolerance mechanism

--- src/index.ts
+++ src/index.ts
@@ -10,15 +10,20 @@ if (cluster.isMaster) {
     console.log(`Master started. Pid: ${pid}`);
     for (let i = 0; i < cpuCount-1; i++) {
         const worker = cluster.fork();
-        worker.on('exit', () => {
-            console.log(`Worker terminated! Pid: ${worker.process.pid}`)
-            cluster.fork();
-        });
+
         worker.send("Hello from server!");
         worker.on('message', (message) => {
             console.log(`Message from worker ${worker.process.pid}: ${JSON.stringify(message)}`)
         });
     }
+
+    cluster.on('exit', (worker, code) => {
+        console.log(`Worker terminated! Pid: ${worker.process.pid} Code: ${code}`);
+        // Restart worker only after specific error
+        if (code === 1) {
+            cluster.fork();
+        }
+    });
 }
 
 if (cluster.isWorker) {
