31f3430716417a8ac694f9543d750391db1df54e
arcbjorn <arky.happ@gmail.com>
2021-05-20 21:12:50 +0300

    feat(serverWorker): add system SIG error handlers

--- src/serverWorker.ts
+++ src/serverWorker.ts
@@ -3,8 +3,32 @@ const pid = process.pid;
 
 // implement worker for cluster
 
-http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
     res.end('Hello from Node.js \n')
 }).listen(8080, () => {
     console.log(`Server started. Pid: ${pid}`);
+});
+
+process.on('SIGINT', () => {
+    console.log('Signal recieved is SIGINT');
+    // Gracefull shutdown
+    server.close(() => {
+        process.exit(0)
+    });
+});
+
+process.on('SIGTERM', () => {
+    console.log('Signal recieved is SIGTERM');
+    // Gracefull shutdown
+    server.close(() => {
+        process.exit(0)
+    });
+});
+
+process.on('SIGUSR2', () => {
+    console.log('Signal recieved is SIGUSR2');
+    // Gracefull shutdown
+    server.close(() => {
+        process.exit(1)
+    });
 });
